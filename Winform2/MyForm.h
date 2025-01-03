fed1b2aa0e1e954c28d0c6eac151fe305c5de9b5
aragami3070 <smirnov17612@gmail.com>
2025-01-03 10:49:13 +0400

    Fix: names errPr in Form2. Add: ReadOnly on txtOut

--- Winform2/MyForm.h
+++ Winform2/MyForm.h
@@ -36,21 +36,30 @@ namespace Winform2 {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Label^ lblFor1;
+	private: System::Windows::Forms::Label^ lblFormula1;
+	private: System::Windows::Forms::Label^ lblFormula2;
+	private: System::Windows::Forms::Label^ lblFormula3;
+	protected:
+
+
 	protected:
 
 	protected:
-	private: System::Windows::Forms::Label^ label2;
-	private: System::Windows::Forms::Label^ label3;
+
+
 	private: System::Windows::Forms::Label^ lblInputX;
 	private: System::Windows::Forms::Label^ lblInputY;
-	private: System::Windows::Forms::ErrorProvider^ errorProvider1;
+	private: System::Windows::Forms::ErrorProvider^ errPrX;
+
+
 	private: System::Windows::Forms::Button^ btnStart;
 
 	private: System::Windows::Forms::TextBox^ txtOut;
 	private: System::Windows::Forms::TextBox^ txtInY;
 	private: System::Windows::Forms::TextBox^ txtInX;
-	private: System::Windows::Forms::ErrorProvider^ errorProvider2;
+	private: System::Windows::Forms::ErrorProvider^ errPrY;
+
+
 	private: System::ComponentModel::IContainer^ components;
 
 	private:
@@ -67,54 +76,54 @@ namespace Winform2 {
 		void InitializeComponent(void)
 		{
 			this->components = (gcnew System::ComponentModel::Container());
-			this->lblFor1 = (gcnew System::Windows::Forms::Label());
-			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->lblFormula1 = (gcnew System::Windows::Forms::Label());
+			this->lblFormula2 = (gcnew System::Windows::Forms::Label());
+			this->lblFormula3 = (gcnew System::Windows::Forms::Label());
 			this->lblInputX = (gcnew System::Windows::Forms::Label());
 			this->lblInputY = (gcnew System::Windows::Forms::Label());
-			this->errorProvider1 = (gcnew System::Windows::Forms::ErrorProvider(this->components));
+			this->errPrX = (gcnew System::Windows::Forms::ErrorProvider(this->components));
 			this->txtInX = (gcnew System::Windows::Forms::TextBox());
 			this->txtInY = (gcnew System::Windows::Forms::TextBox());
 			this->txtOut = (gcnew System::Windows::Forms::TextBox());
 			this->btnStart = (gcnew System::Windows::Forms::Button());
-			this->errorProvider2 = (gcnew System::Windows::Forms::ErrorProvider(this->components));
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->errorProvider1))->BeginInit();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->errorProvider2))->BeginInit();
+			this->errPrY = (gcnew System::Windows::Forms::ErrorProvider(this->components));
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->errPrX))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->errPrY))->BeginInit();
 			this->SuspendLayout();
 			// 
-			// lblFor1
+			// lblFormula1
 			// 
-			this->lblFor1->AutoSize = true;
-			this->lblFor1->Font = (gcnew System::Drawing::Font(L"Linux Biolinum G", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->lblFormula1->AutoSize = true;
+			this->lblFormula1->Font = (gcnew System::Drawing::Font(L"Linux Biolinum G", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->lblFor1->Location = System::Drawing::Point(21, 137);
-			this->lblFor1->Name = L"lblFor1";
-			this->lblFor1->Size = System::Drawing::Size(116, 22);
-			this->lblFor1->TabIndex = 0;
-			this->lblFor1->Text = L"y^2 * sin(x^2)";
+			this->lblFormula1->Location = System::Drawing::Point(21, 137);
+			this->lblFormula1->Name = L"lblFormula1";
+			this->lblFormula1->Size = System::Drawing::Size(116, 22);
+			this->lblFormula1->TabIndex = 0;
+			this->lblFormula1->Text = L"y^2 * sin(x^2)";
 			// 
-			// label2
+			// lblFormula2
 			// 
-			this->label2->AutoSize = true;
-			this->label2->Font = (gcnew System::Drawing::Font(L"Linux Biolinum G", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->lblFormula2->AutoSize = true;
+			this->lblFormula2->Font = (gcnew System::Drawing::Font(L"Linux Biolinum G", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label2->Location = System::Drawing::Point(12, 150);
-			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(156, 22);
-			this->label2->TabIndex = 1;
-			this->label2->Tag = L"";
-			this->label2->Text = L"---------------------  =";
+			this->lblFormula2->Location = System::Drawing::Point(12, 150);
+			this->lblFormula2->Name = L"lblFormula2";
+			this->lblFormula2->Size = System::Drawing::Size(156, 22);
+			this->lblFormula2->TabIndex = 1;
+			this->lblFormula2->Tag = L"";
+			this->lblFormula2->Text = L"---------------------  =";
 			// 
-			// label3
+			// lblFormula3
 			// 
-			this->label3->AutoSize = true;
-			this->label3->Font = (gcnew System::Drawing::Font(L"Linux Biolinum G", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->lblFormula3->AutoSize = true;
+			this->lblFormula3->Font = (gcnew System::Drawing::Font(L"Linux Biolinum G", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label3->Location = System::Drawing::Point(41, 170);
-			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(67, 22);
-			this->label3->TabIndex = 2;
-			this->label3->Text = L"x + y^2";
+			this->lblFormula3->Location = System::Drawing::Point(41, 170);
+			this->lblFormula3->Name = L"lblFormula3";
+			this->lblFormula3->Size = System::Drawing::Size(67, 22);
+			this->lblFormula3->TabIndex = 2;
+			this->lblFormula3->Text = L"x + y^2";
 			// 
 			// lblInputX
 			// 
@@ -138,9 +147,9 @@ namespace Winform2 {
 			this->lblInputY->TabIndex = 4;
 			this->lblInputY->Text = L"¬ведите Y:";
 			// 
-			// errorProvider1
+			// errPrX
 			// 
-			this->errorProvider1->ContainerControl = this;
+			this->errPrX->ContainerControl = this;
 			// 
 			// txtInX
 			// 
@@ -167,6 +176,7 @@ namespace Winform2 {
 			this->txtOut->Location = System::Drawing::Point(174, 103);
 			this->txtOut->Multiline = true;
 			this->txtOut->Name = L"txtOut";
+			this->txtOut->ReadOnly = true;
 			this->txtOut->Size = System::Drawing::Size(266, 116);
 			this->txtOut->TabIndex = 7;
 			// 
@@ -182,9 +192,9 @@ namespace Winform2 {
 			this->btnStart->UseVisualStyleBackColor = true;
 			this->btnStart->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
-			// errorProvider2
+			// errPrY
 			// 
-			this->errorProvider2->ContainerControl = this;
+			this->errPrY->ContainerControl = this;
 			// 
 			// MyForm
 			// 
@@ -197,13 +207,13 @@ namespace Winform2 {
 			this->Controls->Add(this->txtInX);
 			this->Controls->Add(this->lblInputY);
 			this->Controls->Add(this->lblInputX);
-			this->Controls->Add(this->label3);
-			this->Controls->Add(this->lblFor1);
-			this->Controls->Add(this->label2);
+			this->Controls->Add(this->lblFormula3);
+			this->Controls->Add(this->lblFormula1);
+			this->Controls->Add(this->lblFormula2);
 			this->Name = L"MyForm";
 			this->Text = L"ѕростые вычислени€";
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->errorProvider1))->EndInit();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->errorProvider2))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->errPrX))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->errPrY))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -212,8 +222,8 @@ namespace Winform2 {
 
 	private: void ClearAll() { // очистка полей
 		this->txtOut->Text = "";
-		errorProvider1->SetError(txtInX, String::Empty);
-		errorProvider2->SetError(txtInY, String::Empty);
+		errPrX->SetError(txtInX, String::Empty);
+		errPrY->SetError(txtInY, String::Empty);
 	}
 		
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
@@ -225,20 +235,20 @@ namespace Winform2 {
 		bool parseY = Int64::TryParse(this->txtInY->Text, InputY);
 		// ввели не число
 		if (!parseX) {
-			errorProvider1->SetError(txtInX, "¬ведено не целое число");
+			errPrX->SetError(txtInX, "¬ведено не целое число");
 			if (!parseY) {
-				errorProvider2->SetError(txtInY, "¬ведено не целое число");
+				errPrY->SetError(txtInY, "¬ведено не целое число");
 			}
 		}
 		else if (!parseY) {
-			errorProvider2->SetError(txtInY, "¬ведено не целое число");
+			errPrY->SetError(txtInY, "¬ведено не целое число");
 		}
 		// проверка на деление на ноль
 		else {
 			if ((InputX == 0 && InputY == 0) || 
 				(InputX == -4 && abs(InputY) == 2)) {
-				errorProvider1->SetError(txtInX, "ƒеление на ноль");
-				errorProvider2->SetError(txtInY, "ƒеление на ноль");
+				errPrX->SetError(txtInX, "ƒеление на ноль");
+				errPrY->SetError(txtInY, "ƒеление на ноль");
 			}
 			else{
 				// все нормально
